29281622557ab97ebd5f665cbe7616ecc0ce9441
sosiskins <117184907+sosiskins@users.noreply.github.com>
2022-11-12 18:14:34 +0300

    Add files via upload

---
+++ 12б11.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+int main() {
+	using namespace std;
+	//variant 1
+/*
+	int a[10];
+	int pr = 1;
+	for (int i = 0; i < 10; i++) {
+		a[i] = rand() % 21;
+		pr *= a[i];
+	}
+	cout << pr << endl;
+	//2
+	int arr[8];
+	int sum = 0;
+	for (int i = 0; i < 8 i++); {
+		arr[i] = rand() % 21 - 10;
+		if (arr[i] > 0) {
+			sum += arr[i];
+		}
+	}
+	for (int i = 0; i < 7; i++)
+	{
+		cout << endl;
+		cout << sum << endl;
+		//3
+		int arr[16];
+		int k = 0;
+		for (int i = 0; i < 16; i++)
+		{
+			arr[i] = rand() % 21;
+			if (arr[i] % 3 == 0 && arr[i] % 5 == 0) {
+				k++;
+			}
+		}
+		for (int i = 0; i < 16; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		cout << k << endl;
+		//4
+		int arr[9];
+		int sum = 0;
+		for (int i = 0; i < 9; I++) {
+			arr[i] = rand() % 21 - 10; 
+			if (arr[i] % 2 == 0) 
+			{ 
+			sum += arr[i];
+		}
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		cout << sum << endl;
+
+}
+
+}*/
